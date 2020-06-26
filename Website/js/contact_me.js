adcc1f808c42771f120610193f583e33cd15fe53
AsepMo <asepmo.story@gmail.com>
2020-06-26 10:58:13 +0700

    Update Contact_Me.php terhubung dengan php hosting di 000webhost.com

--- Website/js/contact_me.js
+++ Website/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://github.com/AWeb41/AsepMo/blob/master/Website/mail/contact_me.php",
+        url: "http://asepmo-story.000webhostapp.com/mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
